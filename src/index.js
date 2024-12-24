92d1c76427ccce8226be5990dfaa8a551bba4c03
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-12-24 17:58:40 +0200

    finish html and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("pizzaa");
+import './style.css';
+import joulupöytä from './assets/restaurant.jpg';
+
+const contentContainer = document.getElementById('content');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Jouluruoka.com';
+contentContainer.appendChild(h1);
+
+const img = document.createElement('img');
+img.src = joulupöytä;
+img.setAttribute('alt', 'Joulupöytä');
+img.className = 'restaurant-img';
+contentContainer.appendChild(img);
+
+const p = document.createElement('p');
+p.textContent = 'Kinkkua, laatikoita ja hetelmiä';
+contentContainer.appendChild(p);
